40ace0b1344f6f2a95e59a099cfb9286cf52b921
Stewart Burton <stewart.burton84@gmail.com>
2025-04-27 23:30:19 +0200

    fix: modal and fade-in animation

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ var modal = document.getElementById("contactModal");
 var btn = document.getElementById("contactBtn");
 // Get close button
 var span = document.getElementsByClassName("close")[0];
+// Get form element
+var form = modal.querySelector("form");
+// Get modal content
+var modalContent = modal.querySelector(".modal-content");
 
 // Open modal
 btn.onclick = function() {
@@ -21,3 +25,25 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Handle form submission with success message
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Stop default submit
+
+  // Actually submit the form using fetch
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(form),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      form.innerHTML = "<p style='color: #4CAF50; font-size: 1.2rem;'>Thank you! Your message has been sent successfully.</p>";
+    } else {
+      form.innerHTML = "<p style='color: red;'>Oops! Something went wrong. Please try again later.</p>";
+    }
+  }).catch(error => {
+    form.innerHTML = "<p style='color: red;'>Oops! Something went wrong. Please try again later.</p>";
+  });
+});
